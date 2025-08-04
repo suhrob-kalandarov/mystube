8ac2c4b8825dc078ce359413ae3d4a5657153da3
Suhrob <suhrob_kalandarov@outlook.com>
2025-08-04 21:38:39 +0500

    Initial folders structure

---
+++ src/components/main/main.jsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+import {Box, Container, Stack} from '@mui/material'
+
+const Main = () => {
+  return (
+    <Stack>
+        Category
+    </Stack>
+  )
+}
+
+export default Main
