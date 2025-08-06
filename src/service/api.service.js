1c7098f921fd6696db1fb2191d963e443fc55894
Suhrob <suhrob_kalandarov@outlook.com>
2025-08-06 19:40:59 +0500

    fix: update API key in `ApiService` configuration

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -6,7 +6,7 @@ const options = {
     maxResults: '50'
   },
   headers: {
-    'x-rapidapi-key': 'e6a6813f39mshc0ab90295c6f7bap1e94b5jsnf6138b62b72b',
+    'x-rapidapi-key': '5c59d73882msh9445a494887b8e1p15c102jsn1e90e5cd1836',
     'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
   }
 };
