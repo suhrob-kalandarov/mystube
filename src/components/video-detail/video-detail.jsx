9b0706ebfef74469ebc358ab2a5f4854f15165a1
Suhrob <suhrob_kalandarov@outlook.com>
2025-08-06 19:03:31 +0500

    feat: enhance `VideoDetail` component with tags, additional stats, and improved layout
    - Add `Chip` components to display video tags with delete icon
    - Include detailed video statistics (views, likes, comments) with icons
    - Add channel avatar, title, and verification icon in video details
    - Improve responsive layout for `ReactPlayer` and `Suggested videos`
    - Update dependencies: `util`, `react-render-html`

--- src/components/video-detail/video-detail.jsx
+++ src/components/video-detail/video-detail.jsx
@@ -1,7 +1,9 @@
-import {useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
-import {ApiService} from "../../service/api.service";
-import {Box} from "@mui/material";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ApiService } from "../../service/api.service";
+import  CheckCircle from "@mui/icons-material/CheckCircle";
+import { Avatar, Box, Chip, Stack, Typography } from "@mui/material";
+import { FavoriteOutlined, MarkChatRead, Tag, Visibility } from "@mui/icons-material";
 import ReactPlayer from 'react-player'
 import {Loader} from "../index";
 
@@ -25,20 +27,67 @@ const VideoDetail = () => {
 
   const {
     snippet: { title, channelId, channelTitle, description, thumbnails, tags},
-    statistics: {viewCount, likeCount, dislikeCount, commentCount},
+    statistics: {viewCount, likeCount, commentCount},
   } = videoDetail
 
   return (
       <Box minHeight={'90vh'} mb={10}>
-        <Box display={'flex'}>
-          <Box width={'75%'}>
+        <Box display={'flex'} sx={{ flexDirection: { xs: 'column', md: 'row' } }}>
+          <Box width={{ xs: '100%', md: '75%'}}>
             <ReactPlayer
                 src={`https://www.youtube.com/watch?v=${id}`}
                 className="react-player"
                 controls
             />
+            {tags && tags.map((item, idx) => (
+                <Chip
+                    label={item}
+                    key={idx}
+                    sx={{
+                      marginTop: '10px',
+                      cursor: 'pointer',
+                      ml: '10px'
+                    }}
+                    deleteIcon={<Tag />}
+                    onDelete={() => {}}
+                    variant='outlined'
+                />
+            ))}
+            <Typography variant='h5' fontWeight='bold' p={2}>
+              {title}
+            </Typography>
+            <Typography variant='subtitle2' p={2} sx={{opacity: 0.7}}>
+              {description}
+            </Typography>
+
+            <Stack direction='row' alignItems='center' gap='20px' py={1} px={2}>
+              <Stack sx={{opacity: 0.7}} direction='row' alignItems='center' gap='3px'>
+                <Visibility />
+                {parseInt(viewCount).toLocaleString('en-US')} views
+              </Stack>
+              <Stack sx={{opacity: 0.7}} direction='row' alignItems='center' gap='3px'>
+                <FavoriteOutlined />
+                {parseInt(likeCount).toLocaleString('en-US')} like
+              </Stack>
+              <Stack sx={{opacity: 0.7}} direction='row' alignItems='center' gap='3px'>
+                <MarkChatRead />
+                {parseInt(commentCount).toLocaleString('en-US')} comments
+              </Stack>
+            </Stack>
+            <Stack direction='row' py={1} px={2}>
+              <Stack direction='row' alignItems='center' gap='5px' marginTop='5px'>
+                <Avatar
+                    alt={channelTitle}
+                    src={thumbnails.default.url}
+                />
+                <Typography variant='subtitle2' color='gray'>
+                  {channelTitle}
+                  <CheckCircle sx={{fontSize: '14px', color: 'gray', ml: '5px', mb: '-2px'}} />
+                </Typography>
+              </Stack>
+            </Stack>
           </Box>
-          <Box width={'25%'}>Suggested videos</Box>
+          <Box width={{ xs: '100%', md: '25%'}}>Suggested videos</Box>
         </Box>
       </Box>
   )
--- src/service/api.service.js
+++ src/service/api.service.js
@@ -6,7 +6,7 @@ const options = {
     maxResults: '50'
   },
   headers: {
-    'x-rapidapi-key': '736db0048cmsh3cd58d6788e617ep18da0cjsn16efe1f68df1',
+    'x-rapidapi-key': 'e6a6813f39mshc0ab90295c6f7bap1e94b5jsnf6138b62b72b',
     'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
   }
 };
